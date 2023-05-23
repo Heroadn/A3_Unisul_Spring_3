3059c21533bf42a73a58ae2e70a90465e66923ea
heroadn <benbenjamin554@gmail.com>
2023-05-23 12:15:29 -0300

    adicionado flyway, cria as tabelas automaticamente

--- src/main/resources/db/migration/V02__criar_e_registrar_pokemon.sql
+++ src/main/resources/db/migration/V02__criar_e_registrar_pokemon.sql
@@ -1,4 +1,7 @@
 create table `pokemon`(
-    `id`  		   INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `nome`         VARCHAR(50)  NOT NULL
+    `id`     INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `nome`   VARCHAR(50)  NOT NULL,
+    `ataque` INTEGER NOT NULL,
+    `defesa` INTEGER NOT NULL,
+    `tipo`   VARCHAR(100)
 );
