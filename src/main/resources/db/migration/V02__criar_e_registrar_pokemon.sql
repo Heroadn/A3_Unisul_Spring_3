44071cd7a27a54084d42c53cbceeb3d4db77df02
heroadn <benbenjamin554@gmail.com>
2023-06-12 11:36:57 -0300

    bug fix

--- src/main/resources/db/migration/V02__criar_e_registrar_pokemon.sql
+++ src/main/resources/db/migration/V02__criar_e_registrar_pokemon.sql
@@ -1,6 +1,6 @@
 create table `pokemon_atributo`(
     `id`	        INTEGER PRIMARY KEY AUTO_INCREMENT,
-	`sexo`      	CHAR(1)  NOT NULL,
+	`sexo`      	VARCHAR(255)  NOT NULL,
 	`tipo`      	VARCHAR(255)  NOT NULL,
 	`geracao`      	VARCHAR(50)  NOT NULL,
     `ataque_minimo`    INTEGER NOT NULL,
@@ -13,7 +13,7 @@ create table `pokemon`(
     `id`            INTEGER PRIMARY KEY AUTO_INCREMENT,
     `nome`          VARCHAR(50)  NOT NULL,
     `descricao`     VARCHAR(50) ,
-    `data_criacao` DATE NOT NULL,
+    `data_criacao`  DATE NOT NULL,
     `atributo_id`	INTEGER  NOT NULL,
      FOREIGN KEY (atributo_id) REFERENCES pokemon_atributo(id)
 );
